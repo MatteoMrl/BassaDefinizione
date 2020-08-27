4221115df9d259ecf3bf5f9867b275cc518636b5
Matteo <ciaomare>
2020-08-27 16:57:03 +0200

    improvements to the 'films to watch' part

--- dist/index.dev.js
+++ dist/index.dev.js
@@ -6,7 +6,44 @@ var express = require('express');
 
 var hbs = require('hbs');
 
-var app = express(); // Define paths for Express config
+var request = require('request');
+
+var app = express();
+var count = 0;
+var toWatch = false;
+var twFilms = [{
+  name: "Breaking Bad"
+}, {
+  name: "Game of thrones"
+}, {
+  name: "Interstellar"
+}, {
+  name: "titanic"
+}, {
+  name: "It"
+}, {
+  name: "American gods"
+}, {
+  name: "The simpsons"
+}, {
+  name: "futurama"
+}, {
+  name: "The martian"
+}, {
+  name: "8 mile"
+}];
+
+searchFilm = function searchFilm(name, callback) {
+  var url = "https://www.omdbapi.com/?t=".concat(encodeURIComponent(name), "&apikey=e3e69745");
+  request(url, function (error, res, body) {
+    if (!error && res.statusCode == 200) {
+      var dataFilm = JSON.parse(body);
+      return callback(undefined, dataFilm);
+    } else {
+      return callback("error", dataFilm);
+    }
+  });
+};
 
 var publicDirectoryPath = path.join(__dirname, 'public');
 var viewsPath = path.join(__dirname, 'templates/views');
@@ -18,5 +55,33 @@ hbs.registerPartials(partialsPath); // Setup static directory to serve
 
 app.use(express["static"](publicDirectoryPath));
 app.get('', function (req, res) {
-  res.render("index");
-}).listen(3000);
+  if (toWatch == false) {
+    twFilms.forEach(function (value, index) {
+      searchFilm(value.name, function (err, data) {
+        count++;
+
+        if (!err) {
+          twFilms[index] = {
+            imdbID: data.imdbID,
+            title: data.Title,
+            plot: data.Plot,
+            rating: data.imdbRating
+          };
+
+          if (count == twFilms.length) {
+            toWatch = true;
+            res.render("index", {
+              twFilms: twFilms
+            });
+          }
+        }
+      });
+    });
+  } else {
+    res.render("index", {
+      twFilms: twFilms
+    });
+  }
+}).listen(3000, function () {
+  return console.log("Listening on port 3000...");
+});
--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
-
+var request = require('request');
 const app = express()
+var count = 0; 
+var toWatch = false;
 
-// Define paths for Express config
+const twFilms = [{ name: "Breaking Bad"},{ name: "Game of thrones"},{ name: "Interstellar"},{ name: "titanic"},{ name: "It"},{ name: "American gods"},{name: "The simpsons"}, {name: "futurama"}, {name: "The martian"}, {name: "8 mile"}];
+
+searchFilm = (name, callback) =>{
+    let url = `https://www.omdbapi.com/?t=${encodeURIComponent(name)}&apikey=e3e69745`;
+    request(url, function(error, res, body){
+        if(!error && res.statusCode == 200){
+            var dataFilm = JSON.parse(body)
+            return callback(undefined,dataFilm);
+        }
+        else{return callback("error", dataFilm)}
+    });
+}
+
+
+
+
+
 const publicDirectoryPath = path.join(__dirname, 'public')
 const viewsPath = path.join(__dirname, 'templates/views')
 const partialsPath = path.join(__dirname, 'templates/partials')
@@ -18,7 +36,23 @@ hbs.registerPartials(partialsPath)
 app.use(express.static(publicDirectoryPath))
 
 app.get('', (req, res) => {
-    res.render("index");
+    if(toWatch == false){
+        twFilms.forEach((value, index)=>{
+        
+            searchFilm(value.name, (err, data) => {
+                count++;
+                if(!err){
+                    twFilms[index] = {imdbID: data.imdbID, title:data.Title, plot: data.Plot, rating: data.imdbRating}
+                    if(count == twFilms.length){
+                        toWatch = true;
+                        res.render("index", {twFilms});
+                    }
+                }
+            })
+        })
+    } else {
+        res.render("index", {twFilms});
+    }
 })
 
-.listen(3000)
+.listen(3000,()=>console.log("Listening on port 3000..."))
